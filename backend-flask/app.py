3a5084329ae3b0a9c5839c45953cc24e50330f80
femifoly <femifoly@gmail.com>
2023-03-04 20:18:20 +0000

    xray update

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,7 +29,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 # Honeycomb
 provider = TracerProvider()
@@ -44,6 +44,9 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+# Xray
+XRayMiddleware(app, xray_recorder)
 
 # Honeycomb
 FlaskInstrumentor().instrument_app(app)
